ceef68647555a0e126e3931d0a833c484a876294
Jayfablead <121211943+Jayfablead@users.noreply.github.com>
2023-05-23 11:56:34 +0530

    lib/main.dart pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:jicksaw/MyApp.dart';
+
 import 'package:jicksaw/Provider/authprovider.dart';
-import 'package:jicksaw/Questions/gameinfo.dart';
-import 'package:jicksaw/Screen/mainpage2.dart';
+
 import 'package:jicksaw/Screen/splashscreen.dart';
-import 'package:jicksaw/design.dart';
-import 'package:jicksaw/first.dart';
+
 import 'package:jicksaw/jigsaw.dart';
-import 'package:jicksaw/Screen/login.dart';
-import 'package:jicksaw/main%20Pages/rank_page.dart';
-import 'package:jicksaw/main%20Pages/shoppage.dart';
-import 'package:jicksaw/modalview.dart';
-import 'package:jicksaw/second.dart';
-import 'package:jicksaw/shake.dart';
+
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 
@@ -27,37 +20,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // ignore: unused_local_variable
     final puzzleKey = GlobalKey<JigsawWidgetState>();
 
-    return Sizer(
-        builder: (context, orientation, deviceType) {
-          return MultiProvider(
-            providers:[
-              ChangeNotifierProvider(create: (context) => authprovider()),
-            ],
-            child: GetMaterialApp(
-                title: 'Jigsaw',
-                debugShowCheckedModeBanner: false,
-                theme: ThemeData(
-                  // This is the theme of your application.
-                  //
-                  // Try running your application with "flutter run". You'll see the
-                  // application has a blue toolbar. Then, without quitting the app, try
-                  // changing the primarySwatch below to Colors.green and then invoke
-                  // "hot reload" (press "r" in the console where you ran "flutter run",
-                  // or simply save your changes to "hot reload" in a Flutter IDE).
-                  // Notice that the counter didn't reset back to zero; the application
-                  // is not restarted.
-                  primarySwatch: Colors.blue,
-                ),
-
-              // for all route info check routs.txt
-
-                home: SplashScreen()
+    return Sizer(builder: (context, orientation, deviceType) {
+      return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => authprovider()),
+        ],
+        child: GetMaterialApp(
+            title: 'Jigsaw',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+            
+              primarySwatch: Colors.blue,
             ),
-          );
-        }
-    );
+
+
+            home: const SplashScreen()),
+      );
+    });
   }
 }
-
